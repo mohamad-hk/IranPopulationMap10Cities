44ba4db86d1455159a3394c005af9ed5805306d2
mohammad hossein karimi <hm.karimi.23179@gmail.com>
2025-05-06 22:39:47 +0330

    Convert DMS to coordinate

--- ConvertToCoordinate.py
+++ ConvertToCoordinate.py
@@ -2,15 +2,30 @@ import math
 import re
 
 
-def dms_to_decimal():
-    const_dms = "38° 4′ 26"
-    parts = const_dms.split(" ")
+def dms_to_decimal(lat_dms,lon_dms):
+    #lat_dms = "38° 4′ 26"
+    #lon_dms = "46° 17′ 46"
+    lat_parts = lat_dms.split(" ")
+    lon_parts = lon_dms.split(" ")
+    lat_degree, lat_minute, lat_second = extract_Fomrat(lat_parts)
+    lon_degree, lon_minute, lon_second = extract_Fomrat(lon_parts)
+
+    lat_decimal = lat_degree + (lat_minute / 60) + (lat_second / 3600)
+    lon_decimal = lon_degree + (lon_minute / 60) + (lon_second / 3600)
+    print(to_Coordinate(lat_decimal, lon_decimal))
+
+
+def extract_Fomrat(parts):
     degree = int(re.sub(r'[^0-9]', "", parts[0]))
     minute = int(re.sub(r'[^0-9]', "", parts[1]))
     second = int(re.sub(r'[^0-9]', "", parts[2]))
-    decimal = degree + (minute / 60) + (second / 3600)
-    print(decimal)
+    return degree,minute,second
 
 
+def to_Coordinate(lat_deg, lon_deg, R=6378137):
+    lon_rad = math.radians(lon_deg)
+    lat_rad = math.radians(lat_deg)
 
-dms_to_decimal()
+    x = R * lon_rad
+    y = R * math.log(math.tan(math.pi / 4 + lat_rad / 2))
+    return x, y
